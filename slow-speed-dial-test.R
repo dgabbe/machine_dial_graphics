b355ca884037832a78f576ca94dc4eb64f91b8ef
David G <dgabbe@acm.org>
2019-12-23 09:42:29 -0500

    Generate new paper dial.

--- slow-speed-dial-test.R
+++ slow-speed-dial-test.R
@@ -1,30 +1,49 @@
-library(circlize)
+suppressPackageStartupMessages(library(circlize))
+
+mm_to_in <- function(mm) {return(mm / 25.4)}
+pt_to_in <- function(pt) {return(pt / 72)}
 
 speeds1 <- c(700, 800, 1000, 1200, 1400, 1600, 1800, 2000, 2235)
 speeds2 <- c(480, 600, 800, 1000, 1200, 1400, 1530)
 speeds3 <- c(300, 400, 500, 600, 700, 800, 965)
 speeds4 <- c(170, 200, 250, 300, 350, 400, 450, 500, 555)
+mounting_hole_dia_in <- 0.375
+dial_outer_edge_dia_in <- 3.426
+dial_gap_mm <- 2 # leave space around outside of dial
+dial_diameter_in <- mm_to_in(45 + 2 * dial_gap_mm)
 dial_rotation <- 312 # degrees
 start_angle <- 270 - (360 - dial_rotation)/2
 stop_angle <- 270 + (360 - dial_rotation)/2
+spindle_pulley_diameters <- c(7.25, 6.25, 5.25, 4.5)
 
 # Code from circlize-test-run chunk
+# Define some parameters
+font_size <- 12 #pt
+scale_height <-
+  (dial_outer_edge_dia_in - dial_diameter_in) / length(spindle_pulley_diameters)
+gap_after = stop_angle - start_angle
 
-circos.clear() # remember to always start w/this call
-font_size <- 14 #pt
-
-# Outermost track/highest speeds
+# Start the dial
+# circos.clear() # remember to always start w/this call
 circos.par(
   clock.wise = TRUE,
   start.degree = start_angle,
-  gap.after = stop_angle - start_angle
+  gap.after = gap_after
 )
+
+# Outermost track/highest speeds
 circos.initialize(factors = "speeds1", xlim = c(min(speeds1), max(speeds1)))
-circos.track(ylim = c(0, 1), bg.col = "light blue")
-circos.text(speeds1, rep(0.6, length(speeds1)), speeds1, facing = "outside", niceFacing = TRUE, cex = fontsize(font_size), font = 2, family = "serif")
+circos.track(
+  ylim = c(0, 1),
+  bg.border = "light blue",
+  track.height = convert_length(scale_height, "in")
+)
+circos.text(speeds1, rep(0.5, length(speeds1)), speeds1, facing = "outside", niceFacing = TRUE, cex = fontsize(font_size), font = 2, family = "serif")
 circos.clear()
 
+# plot exists at this point.
 # Add center mark
+# par(new = TRUE)
 lines(x = c(-0.1, 0.1), y = c(0, 0), fg = "lightgray")
 lines(x = c(0, 0), y = c(-0.1, 0.1), fg = "lightgray")
 
@@ -33,7 +52,7 @@ symbols(c(0, 0), c(0, 0),
         circles = c(dial_diameter_in / 2, mounting_hole_dia_in / 2),
         fg = "yellow", add = TRUE)
 
-#circlize:::get_most_inside_radius()
+#circlize:::get_most_inside_radius() # For better setting canvas limits
 
 # 2nd highest speeds
 par(new = TRUE) # See code for fig 6.4, "Circular Visualization in R", https://jokergoo.github.io/circlize_book/book/advanced-layout.html#arrange-multiple-plots
@@ -42,17 +61,18 @@ circos.par(
   canvas.ylim = c(-1.2, 1.2),
   clock.wise = TRUE,
   start.degree = start_angle,
-  gap.after = stop_angle - start_angle
+  gap.after = gap_after
 )
 circos.initialize(factors = "speeds2",
                   xlim = c(min(speeds2), max(speeds2)))
 circos.track(
   ylim = c(0, 1),
-  bg.col = "yellow",
+  bg.border = "yellow",
+  track.height = convert_length(scale_height, "in"),
   panel.fun = function(x, y) {
     circos.text(
       speeds2,
-      rep(0.6, length(speeds2)),
+      rep(0.5, length(speeds2)),
       speeds2,
       facing = "outside",
       niceFacing = TRUE,
@@ -62,44 +82,58 @@ circos.track(
     )
   }
 )
+circos.par()
 circos.clear()
 
 # 3nd highest speeds
 par(new = TRUE)
 circos.par(
-  canvas.xlim = c(-1.4, 1.4),
-  canvas.ylim = c(-1.4, 1.4),
+  canvas.xlim = c(-1.55, 1.55),
+  canvas.ylim = c(-1.55, 1.55),
   clock.wise = TRUE,
   start.degree = start_angle,
-  gap.after = stop_angle - start_angle
+  gap.after = gap_after
 )
 circos.initialize(factors = "speeds3",
                   xlim = c(min(speeds3), max(speeds3)))
-circos.track(ylim = c(0, 1),
-             bg.col = "red",
-             panel.fun = function(x, y) {
-               circos.text(speeds3, rep(0.6, length(speeds3)), speeds3, facing = "outside", niceFacing = TRUE, cex = fontsize(font_size), font = 2, family = "serif")
-             }
+circos.track(
+  ylim = c(0, 1),
+  bg.border = "red",
+  track.height = convert_length(scale_height, "in"),
+  panel.fun = function(x, y) {
+    circos.text(
+      speeds3,
+      rep(0.5, length(speeds3)),
+      speeds3,
+      facing = "outside",
+      niceFacing = TRUE,
+      cex = fontsize(font_size),
+      font = 2,
+      family = "serif"
+    )
+  }
 )
 circos.clear()
 
 # 4th highest speeds
 par(new = TRUE)
 circos.par(
-  canvas.xlim = c(-1.6, 1.6),
-  canvas.ylim = c(-1.6, 1.6),
+  canvas.xlim = c(-2.1, 2.1),
+  canvas.ylim = c(-2.1, 2.1),
   clock.wise = TRUE,
   start.degree = start_angle,
-  gap.after = stop_angle - start_angle
+  gap.after = gap_after
 )
 circos.initialize(factors = "speeds4",
                   xlim = c(min(speeds4), max(speeds4)))
 circos.track(
   ylim = c(0, 1),
+  bg.border = "purple",
+  track.height = convert_length(scale_height, "in"),
   panel.fun = function(x, y) {
     circos.text(
       speeds4,
-      rep(0.6, length(speeds4)),
+      rep(0.5, length(speeds4)),
       speeds4,
       facing = "outside",
       niceFacing = TRUE,
@@ -110,3 +144,4 @@ circos.track(
   }
 )
 circos.clear()
+
