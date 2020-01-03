41761af3f71ea0d811128298d272315e9428fc93
David G <dgabbe@acm.org>
2020-01-03 11:23:37 -0500

    Removed track bg.color; refactored dial features other than scales into functions. Not all functions populated yet. Moved per dial parameters to beginning of file.

--- dial-test.R
+++ dial-test.R
@@ -5,42 +5,74 @@ library(tibble)
 mm_to_in <- function(mm) {return(mm / 25.4)}
 pt_to_in <- function(pt) {return(pt / 72)}
 
+#
+# Change these parameters for the dial being made
+#
+is_save_to_file <- FALSE
+mounting_hole_dia_in <- 0.375
+dial_gap_mm <- 2 # leave space around outside of dial
+dial_diameter_in <- mm_to_in(45 + 2 * dial_gap_mm)
+
+font_size <- 15 #pt
+scale_height <- pt_to_in(1.6 * font_size)
+font_family <- "DIN Alternate"
+# font_family <- "DIN 1451 fette Breitschrift 1936"
+
+
+#
+# Helper functions
+#
+draw_outer_border <- function() {
+  # There is another 4% available, check par("usr")
+  x0 <- 0
+  x1 <- 1
+  y0 <- 0
+  y1 <- 1
+  lines(x = c(x0, x1), y = c(y0, y0), col = "lightgray") # bottom
+  lines(x = c(x1, y0), y = c(x1, y1), col = "lightgray") # top
+  lines(x = c(x0, x0), y = c(y0, y1), col = "lightgray") # left side
+  lines(x = c(x1, x1), y = c(y0, y1), col = "lightgray") # right side
+}
+
+draw_center_mark <- function(mounting_hole_dia = mounting_hole_dia_in) {}
+
+draw_dial_boundary <- function(dia = dial_diameter_in) {}
+
+draw_credit <- function() {}
+
 speeds1 <- c(700, 800, 1000, 1200, 1400, 1600, 1800, 2000, 2235)
 speeds2 <- c(480, 600, 800, 1000, 1200, 1400, 1530)
 speeds3 <- c(300, 400, 500, 600, 700, 800, 965)
 speeds4 <- c(170, 200, 250, 300, 350, 400, 450, 500, 555)
 
-dial_scales <- tribble( ~band, ~speeds, ~bg_color,
-                   1, speeds1, "light blue",
-                   2, speeds2, "yellow",
-                   3, speeds3, "red",
-                   4, speeds4, "purple")
+dial_scales <- tribble(
+  ~ band, ~ speeds,
+  1, speeds1,
+  2, speeds2,
+  3, speeds3,
+  4, speeds4
+)
 
-xy_canvas_lims <- c(1, 1.16, 1.4,  1.72)
+xy_canvas_lims <- c(1, 1.16, 1.4, 1.72) # to nest independent plots
+# xy_canvas_lims <- c(1, 1.16, 1.2, 1.4)
+dial_scales <- add_column(dial_scales, bg_color = c("light blue", "yellow", "red", "purple")) # debugging
 dial_scales <- add_column(dial_scales, xy_canvas_lim = xy_canvas_lims)
 dial_scales <- add_column(dial_scales, type_adj = c(16, 12, 9, 7))
 dial_scales <- add_column(dial_scales, line_width = c(4, 3, 2.5, 1.75))
 dial_scales <- add_column(dial_scales, line_color = c("#000000", "#222222", "#444444", "#666666"))
 
-mounting_hole_dia_in <- 0.375
-dial_outer_edge_dia_in <- 3.426 # 246.67px
-dial_gap_mm <- 2 # leave space around outside of dial
-dial_diameter_in <- mm_to_in(45 + 2 * dial_gap_mm)
+dial_outer_edge_dia_in <- 3.426 # 246.67px, width of the flat part of the chasis cover
 dial_rotation <- 312 # degrees
 start_angle <- 270 - (360 - dial_rotation)/2
 stop_angle <- 270 + (360 - dial_rotation)/2
 spindle_pulley_diameters <- c(7.25, 6.25, 5.25, 4.5)
 
 # Code from circlize-test-run chunk
-# Define some parameters
-font_size <- 15 #pt
-scale_height <- pt_to_in(1.6 * font_size)
-font_family <- "DIN Alternate"
-# font_family <- "DIN 1451 fette Breitschrift 1936"
+
+# Definitions that should not be changed
 gap_after = stop_angle - start_angle
 cell_padding <- c(0.00, 1.00, 0.00, 1.00)
 
-is_save_to_file <- TRUE
 
 #
 # Start the dial
@@ -55,24 +87,29 @@ if (is_save_to_file) {
 }
 
 # Set up the device
+# Do not include 'new = "TRUE"' because there is no plot yet.
 par(fin = c(dial_outer_edge_dia_in, dial_outer_edge_dia_in),
     pin = c(dial_outer_edge_dia_in, dial_outer_edge_dia_in),
-    mar = c(0, 0, 0, 0),
-    fig = c(0, 1, 0, 1))
+    mai = c(0, 0, 0, 0),
+    fig = c(0, 1, 0, 1) # wonder if this is the problem...
+    )
 
 # Create a new frame so RStudio doesn't overplot
-plot.new()
+plot.new()  # not creating a new plot in RStudio Plot pane...
 
 # draw a border for printing
-# There is another 4% available, check par("usr")
-x0 <- 0
-x1 <- 1
-y0 <- 0
-y1 <- 1
-lines(x = c(x0, x1), y = c(y0, y0), col = "lightgray") # bottom
-lines(x = c(x1, y0), y = c(x1, y1), col = "lightgray") # top
-lines(x = c(x0, x0), y = c(y0, y1), col = "lightgray") # left side
-lines(x = c(x1, x1), y = c(y0, y1), col = "lightgray") # right side
+draw_outer_border()
+
+cross_mark <- mounting_hole_dia_in / 2
+lines(x = c(0.5 - cross_mark, 0.5 + cross_mark), y = c(0.5, 0.5), col = "lightgray")
+lines(x = c(0.5, 0.5), y = c(0.5 - cross_mark, 0.5 + cross_mark), col = "lightgray")
+
+# Outline for shaft hole
+plotrix::draw.circle(
+  c(0.5), c(0.5),
+  radius = c(mounting_hole_dia_in / 2, dial_diameter_in / 2),
+  border = c("lightgray", "lightgray"), lty = c(1, 3)
+)
 
 #
 # Add the RPM scales
@@ -100,9 +137,10 @@ for (i in 1:nrow(dial_scales)) {
 
     circos.track(
     ylim = c(0, 1),
-    bg.border = "white",
+    bg.border = NA, # Use bg_color for debugging
     track.height = convert_length(scale_height, "in"),
     panel.fun = function(x, y) {
+      # Scale numbers
       circos.text(
         speeds,
         rep(0.5, length(speeds)),
@@ -113,6 +151,7 @@ for (i in 1:nrow(dial_scales)) {
         font = 2,
         family = font_family
       )
+      # Scale delineation
       circos.lines(
         x = c(speeds[1] - type_adj, speeds[length(speeds)] + type_adj),
         y = c(1, 1),
@@ -128,6 +167,7 @@ for (i in 1:nrow(dial_scales)) {
 
 
 # Add rectangular boundry, add canvas size
+
 # Add dimensional marks
 par(
   new = TRUE,
@@ -137,15 +177,15 @@ par(
   fig = c(0, 1, 0, 1)
 )
 
-cross_mark <- mounting_hole_dia_in / 2
-lines(x = c(-cross_mark, cross_mark), y = c(0, 0), col = "lightgray")
-lines(x = c(0, 0), y = c(-cross_mark, cross_mark), col = "lightgray")
+# cross_mark <- mounting_hole_dia_in / 2
+# lines(x = c(-cross_mark, cross_mark), y = c(0, 0), col = "lightgray")
+# lines(x = c(0, 0), y = c(-cross_mark, cross_mark), col = "lightgray")
 
-# Outline for shaft hole
-plotrix::draw.circle(c(0), c(0),
-                     radius = c(mounting_hole_dia_in / 2, dial_diameter_in / 2),
-                     border = c("lightgray", "lightgray"), lty = c(1, 3)
-)
+# # Outline for shaft hole
+# plotrix::draw.circle(c(0), c(0),
+#                      radius = c(mounting_hole_dia_in / 2, dial_diameter_in / 2),
+#                      border = c("lightgray", "lightgray"), lty = c(1, 3)
+# )
 
 # faint boundary for dial
 # plotrix::draw.circle(c(0), c(0),
