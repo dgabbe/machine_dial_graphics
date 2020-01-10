b5606dd7bacca3962751b6fe392c6c0c75f54903
David G <dgabbe@acm.org>
2020-01-10 12:10:22 -0500

    Worked on plot diagnostic functions.

--- device-space/plot-regions-corrd-systems.R
+++ device-space/plot-regions-corrd-systems.R
@@ -6,13 +6,23 @@
 #
 
 dial_outer_edge_dia_in <- 3.426 # 246.67px, width of the flat part of the chasis cover
+center <- dial_outer_edge_dia_in / 2
+mounting_hole_dia_in <- 0.375
+mounting_hole_rad <- mounting_hole_dia_in / 2
+dial_diameter_in <- 1.929
 
 x <- c(0, dial_outer_edge_dia_in)
 y <- c(0, dial_outer_edge_dia_in)
+# x <- c(0, 1)
+# y <- c(0, 1)
 
+# Setup parameters for plots
 par(mai = rep(0, 4))
+
+# Create a plot, in this case, an empty one
 plot(x, y, asp = 1, type = "n", ann = FALSE, axes = FALSE)
 
+#explr_graphics()
 #par(no.readonly = TRUE)
 
 box_types <- c("plot", "figure", "inner", "outer")
@@ -22,5 +32,25 @@ box(which = "figure", col = "red")
 box(which = "inner", col = "green")
 box(which = "outer", col = "blue")
 
-abline(h = x/2)
-abline(v = x/2)
+par(new = TRUE)
+plot(c(-center, center), c(-center, center),
+     asp = 1,
+     type = "n",
+     ann = FALSE,
+     axes = FALSE
+)
+
+lines(c(-mounting_hole_rad - 0.06, mounting_hole_rad + 0.06), c(0, 0))
+lines(c(0, 0), c(-mounting_hole_rad - 0.06, mounting_hole_rad + 0.06))
+
+plotrix::draw.circle(
+  0, 0,
+  radius = mounting_hole_rad,
+  border = "lightgray", lty = 1
+)
+
+plotrix::draw.circle(
+  0, 0,
+  radius = dial_diameter_in / 2,
+  border = "lightgray", lty = 3
+)
---
+++ explr_graphics.R
@@ -0,0 +1,22 @@
+explr_graphics <- function() {
+  dev.list()
+  dev.cur()
+  dev.size(units = "in")
+  par("din")
+  par("mai")
+  par("pin")
+  par("usr")
+}
+
+par_decoder <- function() {
+  # decode the graphics parameters
+  # device size
+  # aspect ratio
+  # panels
+  # figure or plot
+  # margins
+  # plot area
+  # usr coordinates
+
+
+}
