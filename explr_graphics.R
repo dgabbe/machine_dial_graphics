7dff06c470f09e470f8e1bb72ea6363280f1fbbc
David G <dgabbe@acm.org>
2020-01-11 21:38:12 -0500

    Updated presentation.

--- explr_graphics.R
+++ explr_graphics.R
@@ -1,3 +1,4 @@
+library(tibble)
 
 explr_graphics <- function() {
   dev.list()
@@ -11,12 +12,28 @@ explr_graphics <- function() {
 
 par_decoder <- function() {
   p <- par()
-  groups <- list("device", "panel", "figure", "margin", "plot", "usr", "annotation", "axes", "elements")
+  groups <- list("device", "panel", "figure", "margin", "plot", "usr", "annotation", "axes", "elements", "text")
+
+  decoder <- tribble(
+    ~param, ~group, ~short_def,
+    "adj", "text", "determines the way in which text strings are justified in text, mtext and title",
+    "ann", "annotation", " annotate the plots with axis titles and overall titles",
+    "ask", "", "deprecated - use devAskNewPage",
+    "bg", "device", "background color of device region",
+    "bty", "plot", "type of box drawn around plots",
+    "cex", "text", "relative magnification of text & symbols to default",
+    "cex.axis", "axes", "relative magnification of axis notation to cex",
+    "cex.lab", "axes", "relative magnification of x & y labels to cex"
+  )
+
   message("Device params")
   message("Panel params")
   message("Margin params")
   message("Plot params")
   message("Usr coordinates")
+
+
+
   # decode the graphics parameters
   # device size
   # aspect ratio
@@ -25,4 +42,9 @@ par_decoder <- function() {
   # margins
   # plot area
   # usr coordinates
+}
+
+par_verify <- function() {
+  # compare number of items in par_decoder and par()
+  message("Coming soon...")
 }
