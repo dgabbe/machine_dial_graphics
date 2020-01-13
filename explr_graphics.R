e0d3ea248e7eb0adc8090f50f8dd2612a472a8d9
David G <dgabbe@acm.org>
2020-01-13 12:52:27 -0500

    Morning updates.

--- explr_graphics.R
+++ explr_graphics.R
@@ -1,9 +1,14 @@
 library(tibble)
 
 explr_graphics <- function() {
-  dev.list()
-  dev.cur()
-  dev.size(units = "in")
+  d <- dev.list()
+  dn <- names(d)
+  dc <- dev.cur()
+  dcn <- names(dev.cur()) # which(dcn%in%dn) to add current dev notation
+  message(paste("devices:  ", appendLF = FALSE))
+#          , d[[1]], ":", dn[[1]]))
+
+    dev.size(units = "in") # format paste(dev.size(units = "in"))
   par("din")
   par("mai")
   par("pin")
@@ -28,11 +33,69 @@ par_decoder <- function() {
     "cex.sub", "axes", "TBI", FALSE,
     "cin", "text", "character size - inches. Same as 'cra'", TRUE,
     "col", "plot", "default plotting color", FALSE,
-    # other col.*
-    "cra", "text", "character size - pixels. Same as 'cin'", TRUE
-
+    "col.axis", "axes", "add description", FALSE,
+    "col.lab", "annotation", "add description", FALSE,
+    "col.main", "annotation", "add description", FALSE,
+    "col.sub", "annotation", "add description", FALSE,
+    "cra", "text", "character size - pixels. Same as 'cin'", TRUE,
+    "crt", "text", "add description", FALSE,
+    "csi", "text", "add description", TRUE,
+    "cxy", "text", "add description", TRUE,
+    "din", "device", "add description", TRUE,
+    "err", "", "Unimplemented", FALSE,
+    "family", "text", "add description", FALSE,
+    "fg", "plot", "add description", FALSE,
+    "fig", "figure", "add description", FALSE,
+    "fin", "figure", "add description", FALSE,
+    "font", "text", "add description", FALSE,
+    "font.axis", "axes", "add description", FALSE,
+    "font.lab", "annotation", "add description", FALSE,
+    "font.main", "annotation", "add description", FALSE,
+    "font.sub", "annotation", "add description", FALSE,
+    "lab", "axes", "add description", FALSE,
+    "las", "axes", "add description", FALSE,
+    "lend", "element", "add description", FALSE,
+    "lheight", "text", "add description", FALSE,
+    "ljoin", "element", "add description", FALSE,
+    "lmitre", "element", "add description", FALSE,
+    "lty", "element", "add description", FALSE,
+    "lwd", "element", "add description", FALSE,
+    "mai", "margin", "add description", FALSE,
+    "mar", "margin", "add description", FALSE,
+    "mex", "text", "add description", FALSE,
+    "mfcol", "panel", "add description", FALSE,
+    "mfg", "panel", "add description", FALSE,
+    "mfrow", "panel", "add description", FALSE,
+    "mgp", "axes", "add description", FALSE,
+    "mkh", "", "Ignored", FALSE,
+    "new", "plot", "add description", FALSE,
+    "oma", "panel", "add description", FALSE,
+    "omd", "panel", "add description", FALSE,
+    "omi", "panel", "add description", FALSE,
+    "page", "plot", "add description", TRUE,
+    "pch", "text", "add description", FALSE,
+    "pin", "plot", "add description", FALSE,
+    "plt", "plot", "add description", FALSE,
+    "ps", "text", "add description", FALSE,
+    "pty", "plot", "add description", FALSE,
+    "smo", "", "Unimplemented", FALSE,
+    "srt", "text", "add description", FALSE,
+    "tck", "axes", "add description", FALSE,
+    "tcl", "axes", "add description", FALSE,
+    "usr", "usr", "add description", FALSE,
+    "xaxp", "axes", "add description", FALSE,
+    "xaxs", "axes", "add description", FALSE,
+    "xaxt", "axes", "add description", FALSE,
+    "xlog", "plot", "add description", FALSE,
+    "xpd", "plot", "add description", FALSE,
+    "yaxp", "axes", "add description", FALSE,
+    "yaxs", "axes", "add description", FALSE,
+    "yaxt", "axes", "add description", FALSE,
+    "ylbias", "axes", "add description", FALSE,
+    "ylog", "plot", "add description", FALSE
   )
 
+  message("Group: param [RO for read-only]: value - brief description")
   message("Device params")
   message("Panel params")
   message("Margin params")
