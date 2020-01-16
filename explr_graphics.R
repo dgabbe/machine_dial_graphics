2e9a5164d3a29102155ae649be93ced907153c7b
David G <dgabbe@acm.org>
2020-01-16 10:17:27 -0500

    Added more descriptions, fixed pretty_print_pars output.

--- explr_graphics.R
+++ explr_graphics.R
@@ -1,10 +1,9 @@
-library(
-  dplyr,
-  tibble)
+library(dplyr)
+library(tibble)
 
 par_params <- tribble(
   ~param, ~group, ~short_def, ~read_only, ~decimals,
-  "adj", "text", "determines the way in which text strings are justified in text, mtext and title", FALSE, 1,
+  "adj", "text", "jtext, mtext and title justification", FALSE, 1,
   "ann", "annotation", " annotate the plots with axis titles and overall titles", FALSE, NA,
   "ask", "", "deprecated - use devAskNewPage", FALSE, NA,
   "bg", "device", "background color of device region", FALSE, NA,
@@ -28,8 +27,8 @@ par_params <- tribble(
   "err", "", "Unimplemented", FALSE, NA,
   "family", "text", "add description", FALSE, NA,
   "fg", "plot", "foreground plot color", FALSE, NA,
-  "fig", "figure", "add description", FALSE, 1,
-  "fin", "figure", "add description", FALSE, 3,
+  "fig", "figure", "figure region in NDC", FALSE, 1,
+  "fin", "figure", "figure region in inches", FALSE, 3,
   "font", "text", "add description", FALSE, 0,
   "font.axis", "axes", "add description", FALSE, 8,
   "font.lab", "annotation", "add description", FALSE, 0,
@@ -45,11 +44,11 @@ par_params <- tribble(
   "lwd", "element", "add description", FALSE, 8,
   "mai", "margin", "margin size in inches", FALSE, 2,
   "mar", "margin", "margin size in lines", FALSE, 1,
-  "mex", "text", "add description", FALSE, 1,
-  "mfcol", "panel", "add description", FALSE, 0,
-  "mfg", "panel", "add description", FALSE, 1,
-  "mfrow", "panel", "add description", FALSE, 0,
-  "mgp", "axes", "add description", FALSE, 8,
+  "mex", "text", "convertion factor between 'mar' & 'mai'", FALSE, 1,
+  "mfcol", "panel", "panel columns", FALSE, 0,
+  "mfg", "panel", "figure being drawn", FALSE, 1,
+  "mfrow", "panel", "panel rows", FALSE, 0,
+  "mgp", "axes", "mex units for margin line", FALSE, 0,
   "mkh", "", "Ignored", FALSE, NA,
   "new", "plot", "not clean frame before drawing", FALSE, NA,
   "oma", "panel", "add description", FALSE, 1,
@@ -66,12 +65,12 @@ par_params <- tribble(
   "tck", "axes", "add description", FALSE, 8,
   "tcl", "axes", "add description", FALSE, 8,
   "usr", "usr", "user coordinates of plotting region", FALSE, 0,
-  "xaxp", "axes", "add description", FALSE, 8,
+  "xaxp", "axes", "coord and interval between tick marks", FALSE, 0,
   "xaxs", "axes", "add description", FALSE, 8,
   "xaxt", "axes", "add description", FALSE, 8,
   "xlog", "plot", "logarithmic scale", FALSE, NA,
   "xpd", "plot", "clip to plot or figure region", FALSE, NA,
-  "yaxp", "axes", "add description", FALSE, 8,
+  "yaxp", "axes", "coord and interval between tick marks", FALSE, 0,
   "yaxs", "axes", "add description", FALSE, 8,
   "yaxt", "axes", "add description", FALSE, 8,
   "ylbias", "axes", "add description", FALSE, 1,
@@ -149,21 +148,22 @@ explr_graphics <- function() {
 }
 
 
-pretty_print_par <- function() {
-  p <- names(par())
+pretty_print_par <- function(p = names(par())) {
+  buffer <- ""
   lapply(
     p,
-    function(p){
+    function(p) {
       p_p <- filter(par_params, param == p)
-      ro <- if (p_p[["read_only"]]) {"[RO]: "} else {": "}
+      ro <- if (p_p[["read_only"]]) {" [RO]: "} else {": "}
       if (!is.na(p_p[["decimals"]]) && is.numeric(p_p[["decimals"]]) == TRUE) {
         v <- paste(format(par(p), digits = 3, nsmall = p_p[["decimals"]]), collapse = " ")
       } else {
         v <- par(p)
       }
-      paste0(p, ro, v, " - ", p_p[["short_def"]])
+      buffer <<- paste0(buffer, paste0(p, ro, v, " - ", p_p[["short_def"]]), "\n" )
     }
-    )
+  )
+  cat(buffer)
 }
 
 par_decoder <- function(params) {
